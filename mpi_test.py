3d1055808cdd1890a2c3ab33c06a982e4f391a6f
Nathaniel <nathaniel.faries@p2c.com>
2015-12-12 14:26:47 -0500

    Pilling the crap higher hoping it doesn't fall over

--- mpi_test.py
+++ mpi_test.py
@@ -3,10 +3,51 @@ from mpi4py import MPI
 comm = MPI.COMM_WORLD
 rank = comm.Get_rank()
 
-if rank == 0:
-	data = [1, 2, 3]
-	comm.send(data, dest=1, tag=11)
-else:
-	data = comm.recv(source=0, tag=11)
+done = False
 
-print "Hi I'm process", rank, "and data is", type(data)
+def increment():
+	global count
+	global done
+	count += 1
+	print rank, "is at", count
+	done = comm.bcast(done, root=1)
+	if done: 
+		print rank, "exiting with count=", count
+		MPI.Finalize()
+
+def main():
+	global done
+	count = 0
+	if rank == 0:
+		while count < 1700:
+			count += 1
+			print rank, "is at", count
+			done = comm.bcast(done, root=1)
+			if done:
+				print rank, "exiting with count=", count
+				
+			# increment()
+	elif rank == 1:
+		while count < 1700:
+			count += 1
+			print rank, "is at", count
+			done = comm.bcast(done, root=1)
+			if done:
+				print rank, "exiting with count=", count
+				
+			# increment()
+		done = True
+		done = comm.bcast(done, root=rank)
+	else:
+		while count < 999:
+			count += 1
+			print rank, "is at", count
+			done = comm.bcast(done, root=1)
+			if done:
+				print rank, "exiting with count=", count
+				
+			# increment()
+	print rank, "exiting from main. Done =", done, "Count =", count
+	MPI.Finalize()
+
+main()
